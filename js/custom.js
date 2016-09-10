840163236f65f742b0011c178a71d2d7c14d3840
Toni Teof <Toni Teof>
2016-09-10 11:13:39 +0200

    navbar finished

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,7 @@
 jQuery(document).ready( function($){
     $('.carousel').carousel();
+    $('.collapsed-image .fa').on('click', function(){
+        $(this).parent().parent().parent().removeClass("in");
+    });
 });
 
